66af4c0b40d01b0cec1fe4f7a9c18b010558f180
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-04-13 15:10:46 +0300

    Add footer, fix styles, add links, add styles reg

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,10 @@ import './Footer.scss'
 const Footer = () => {
   return(
     <div className="Footer">
+      <div className="Footer__container">
+        <span>Соглашение</span>
+        <span>АО Витли Индастрис</span>
+      </div>
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import './Header.scss'
 import logo from '../../img/logo.png'
 import Button from '../UI/Button/Button'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return(
     <div className="Header">
       <div className="Header__container">
-        <img className="Header__logo" src={logo} alt="logo" />
-        <Button text="Авторизация" />
+        <NavLink to='/'>
+          <div className="Header__block">
+            <img className="Header__logo" src={logo} alt="logo" />
+            <h2>Chat</h2>
+          </div>
+        </NavLink>
+        <NavLink to="/registration">
+          <Button text="Авторизация" />
+        </NavLink>
       </div>
     </div>
   )
